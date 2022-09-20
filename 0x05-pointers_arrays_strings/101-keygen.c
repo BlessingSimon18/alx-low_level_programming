fb48e3ce1f99ec954a4d8cad8730b8624b6a47db
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-09-19 20:42:58 -0700

    Task_11-Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,69 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
 
 /**
  * main - entry point
  *
- * Return: generated password
+ * key_gen - password generator
+ *
+ * @N: length of the password
+ *
  */
+
+void key_gen(int N)
+{
+	int i;
+	int r;
+
+	srand((unsigned int)(time(NULL)));
+
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+
+	char password[N];
+
+	r = rand() % 4;
+
+	for (i = 0; i < N; i++)
+	{
+		if (r == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			r = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (r == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			r = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (r == 3)
+		{
+			password[i] = LETTER[rand() % 26];
+			r = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letter[rand() % 26];
+			r = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+
 
 int main(void)
 {
-	char c;
-	int x;
+	int N = 12;
 
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
+	key_gen(N);
 
 	return (0);
 }
