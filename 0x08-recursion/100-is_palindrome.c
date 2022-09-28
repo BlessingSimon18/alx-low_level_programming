9d517c10070a9525d1f4c7935c046adcce44fbb9
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-09-28 06:26:19 -0700

    Task_7-They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
-void check(char *s, int start, int end, int *flag);
-int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
@@ -11,53 +11,15 @@ int _strlen_recursion(char *s);
  * Return: 1 if s is a palindrome, 0 otherwise
  */
 
-int is_palindrome(char *s)
+int find_strlen(char *s)
 {
-	int flag n= 1;
+	int len = 0;
 
-	check(s, 0. _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
-
-
-/**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * @flag: flag to indicate if a string is a palindrome
- *
- * Return: void
- */
-
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
+	if (*(s + len))
 	{
-		if(s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
+		len++;
+		len += find_strlen(s + len);
 	}
-}
-
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- *
- * Return: length of the string
- */
-
-int _strlen_recursion(char *s)
-{
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum ++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
+	
+	return (len);
 }
