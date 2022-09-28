a48c9904002d77235ac36e363de18b2a2705a629
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-09-28 06:13:31 -0700

    Task_6-Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,8 @@ int is_prime_number(int n);
 int is_prime(int n, int start);
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
  * @n: number to be checked
  *
  * Return: 1 if n is prime, 0 otherwise
