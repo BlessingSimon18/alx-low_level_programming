3438f125335ef91f6bdf66130e4ac410bf014a53
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-09-28 05:59:24 -0700

    Task_8-Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,8 @@ char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 
 /**
- * strlen_no_wilds - Returns the length of a string,ignoring wildcard characters
+ * strlen_no_wilds - Returns the length of a string,ignoring wildcard
+ * characters
  * @str: The string to be measured.
  *
  * Return: The length
@@ -28,7 +29,7 @@ int strlen_no_wilds(char *str)
 
 /**
  * iterate_wild - Iterates through a string located at a wildcard
- * 		 until it points to a non-wildcard character.
+ *	until it points to a non-wildcard character.
  * @wildstr: The string to be iterated through.
  */
 void iterate_wild(char **wildstr)
@@ -42,12 +43,13 @@ void iterate_wild(char **wildstr)
 
 /**
  * postfix_match - Checks if a string str matches the postfix of
- * 	another string potentially containing wildcards.
+ *	another string potentially containing wildcards.
  * @str: The string to be matched.
  * @postfix: The postfix.
  *
  * Return: If str and postfix are identical - a pointer to the null byte
- * 	located at the end of postfix.Otherwise - a pointer to the first unmatched character in postfix.
+ *	located at the end of postfix.Otherwise - a pointer to the first unmatched
+ *	character in postfix.
  */
 char *postfix_match(char *str, char *postfix)
 {
@@ -56,7 +58,7 @@ char *postfix_match(char *str, char *postfix)
 
 	if (*postfix == '*')
 		iterate_wild(&postfix);
-	
+
 	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
 	{
 		postfix++;
@@ -82,7 +84,7 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '\0')
 		return (1);
-	
+
 	if (*s1 != *s2)
 		return (0);
 
