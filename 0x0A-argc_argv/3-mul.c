25ff4681a9059257e508733a74008a1d5feb4926
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-09-30 15:46:10 -0700

    Task_2-The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,17 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-	if (argc == 3)
+
+	b = 1;
+	if (argc < 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a *b);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	for (a = 1; a < argc; a++)
+	{
+		b = b * atoi(argv[a]);
+	}
+	printf("%d\n", b);
+	return (0);
 }
