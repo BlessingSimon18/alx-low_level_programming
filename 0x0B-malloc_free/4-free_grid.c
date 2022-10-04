14ea1191ddb5b474b1568e1c4f7d756b4ce741b6
BlessingSimon18 <blessinglilyadagold@gmail.com>
2022-10-04 06:45:00 -0700

    Task_4-It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,22 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_grid - allocates a grid, make space and free space
- * @grid: takes in width of grid
+ * free_grid  - frees up a grid
+ * @grid: grid to check
  * @height: height of grid
- *
- * Return: free grid
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
+
+	if (height <= 0)
+		return;
+
+	if (grid == NULL)
+		return;
 
 	for (i = 0; i < height; i++)
 	{
